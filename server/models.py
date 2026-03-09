7c1afd4884bb901d903cbe7e8e608656d8629a4e
Miguelkabz <kabzmiguel@gmail.com>
2026-03-09 22:55:37 +0300

    done

--- server/models.py
+++ server/models.py
@@ -3,6 +3,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy.orm import backref
 
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
@@ -11,12 +12,34 @@ metadata = MetaData(naming_convention={
 db = SQLAlchemy(metadata=metadata)
 
 
+# Association table for Employee-Meeting many-to-many relationship
+employee_meetings = db.Table(
+    'employee_meetings',
+    db.Column('employee_id', db.Integer, db.ForeignKey('employees.id'), primary_key=True),
+    db.Column('meeting_id', db.Integer, db.ForeignKey('meetings.id'), primary_key=True)
+)
+
+
+# Association table for Employee-Project many-to-many relationship (through assignment)
+employee_projects = db.Table(
+    'employee_projects',
+    db.Column('employee_id', db.Integer, db.ForeignKey('employees.id'), primary_key=True),
+    db.Column('project_id', db.Integer, db.ForeignKey('projects.id'), primary_key=True)
+)
+
+
 class Employee(db.Model):
     __tablename__ = 'employees'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     hire_date = db.Column(db.Date)
+
+    # Many-to-many relationship with meetings
+    meetings = db.relationship('Meeting', secondary=employee_meetings, back_populates='employees')
+    
+    # Many-to-many relationship with projects
+    projects = db.relationship('Project', secondary=employee_projects, back_populates='employees')
 
     def __repr__(self):
         return f'<Employee {self.id}, {self.name}, {self.hire_date}>'
@@ -30,6 +53,9 @@ class Meeting(db.Model):
     scheduled_time = db.Column(db.DateTime)
     location = db.Column(db.String)
 
+    # Many-to-many relationship with employees
+    employees = db.relationship('Employee', secondary=employee_meetings, back_populates='meetings')
+
     def __repr__(self):
         return f'<Meeting {self.id}, {self.topic}, {self.scheduled_time}, {self.location}>'
 
@@ -40,6 +66,9 @@ class Project(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String)
     budget = db.Column(db.Integer)
+
+    # Many-to-many relationship with employees
+    employees = db.relationship('Employee', secondary=employee_projects, back_populates='projects')
 
     def __repr__(self):
         return f'<Review {self.id}, {self.title}, {self.budget}>'
